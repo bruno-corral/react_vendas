25ad822dbdb25a195f90793e5183df76e3e8d99e
Bruno Corral <brunoalcorral@gmail.com>
2023-09-13 21:27:02 -0300

    Removendo métodos sem uso do api.tsx

--- src/app/utils/api.tsx
+++ src/app/utils/api.tsx
@@ -3,13 +3,3 @@ import axios from 'axios';
 export const api = axios.create({
     baseURL: 'http://127.0.0.1:8000/api/'
 });
-
-// export const getVendedores = async () => {
-//     const result = await api.get(`vendedores/all`);
-//     return result.data;
-// }
-
-// export const postVendedor = async () => {
-//     const result = await api.post(`vendedores/add`);
-//     return result.data;
-// }
